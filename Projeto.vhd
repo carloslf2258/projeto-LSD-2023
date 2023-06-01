d177a8962b92512b048054518bdff6779c3c4fe3
SofiaMarrafa <116880858+SofiaMarrafa@users.noreply.github.com>
2023-06-01 16:25:14 +0100

    Update Projeto.vhd

--- Projeto.vhd
+++ Projeto.vhd
@@ -12,13 +12,19 @@ entity Projeto is
         LEDG: out std_logic_vector (2 downto 0);
         HEX4: out std_logic_vector (7 downto 0);
         HEX: out std_logic_vector (7 downto 0);
-        DEC: out std_logic_vector (7 downto 0)
+        DEC: out std_logic_vector (7 downto 0);
+	
     );
 end Projeto;
 
 architecture Behavioral of Projeto is
     -- Declare internal signals here
-    signal RegOutputs: std_logic_vector (4 downto 0);
+    signal RegCozer: std_logic_vector (4 downto 0);
+	 signal RegLevedar: std_logic_vector (4 downto 0);
+	 signal RegAmassar: std_logic_vector (4 downto 0);
+	 
+	 --signal RegExtra: std_logic_vector (4 downto 0);
+	 signal RegStart_Stop: std_logic;
     signal timerExp: std_logic;
     signal newTime, timerEnable: std_logic;
     signal timeValue: integer;
@@ -33,11 +39,11 @@ begin
 		 P2 => not SW(0),
 		 Time_adjust => Time_adjust,
 		 Start_Stop => key0,
-		 Time_extra => RegOutputs(0),
-		 Time_cozer => RegOutputs(1),
-		 Time_levedar => RegOutputs(2),
-		 Time_amassar => RegOutputs(3),
-		 Start_Stop_out => RegOutputs(4)
+		 --Time_extra => RegExtra,
+		 Time_cozer => RegCozer,
+		 Time_levedar => RegLevedar,
+		 Time_amassar => RegAmassar,
+		 Start_Stop_out => RegStart_Stop
 	);
 
 
@@ -45,10 +51,10 @@ begin
     FSM: entity work.FSM(Behavioral) port map (
         clk => clk,
         reset => reset,
-        time_amassar => to_integer(signed(RegOutputs(3))),
-        time_levedar => to_integer(signed(RegOutputs(2))),
-        time_cozer => to_integer(signed(RegOutputs(1))),
-        start_stop => RegOutputs(4),
+        time_amassar => RegAmassar,
+        time_levedar => RegLevedar,
+        time_cozer => RegCozer,
+        start_stop => RegStart_Stop,
         time_expired => timerExp,
         LEDR => LEDR,
         display_select => displaySelect,
@@ -56,6 +62,7 @@ begin
         new_time => newTime,
         time_value => timeValue,
         timer_enable => timerEnable
+	
     );
     -- Instantiate TimerFSM
     TimerFSM: entity work.TimerFSM(Behavioral) port map (
